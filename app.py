a027520229112e45c6b0ef5beb9a6aaabf0f3564
minlingz <minling.zhou@gmail.com>
2023-09-02 21:47:00 +0000

    enhanced app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-#-----------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See LICENSE in the project root for license information.
-#-----------------------------------------------------------------------------------------
+# Licensed under the MIT License.
+# See LICENSE in the project root for license information.
+# --------------------------------------------------------------------------------------
 
 from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
